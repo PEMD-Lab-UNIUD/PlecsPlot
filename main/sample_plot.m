8adcc338f46869fb2c44f1af74398747f8d5daeb
RiccardoBreda <riccardobreda.breda@gmail.com>
2022-12-30 17:57:51 +0100

    First version of the example file

--- main/sample_plot.m
+++ main/sample_plot.m
@@ -2,12 +2,20 @@ clc
 clear
 close all
 
-filename ="TorqueSpeed";
+% filename ="TorqueSpeed";
 
-warning('on','all')
-debugmode(true)
-% outputmode(false)
-save2filemode(false)
+warning('on','all')                                         % to disply the warnings
+debugmode(true)                                             % to enable console output for debug purposes
+save2filemode(false)                                        % to save the plot on file
+
+%% Layout settings
+
+nrows = 4;
+ncols = 4;
+fullcols = ncols;
+halfcols = fullcols/2;
+fullrows = nrows;
+halfrows = fullrows/2;
 
 
 %% Data to plot
@@ -23,28 +31,37 @@ y4 = cos(4*x);
 %% Plot parameter definition
 Options = plotOptions_V2();
 
-%% PLOT TO FILE
+%% Set image title
 ImageTitle = "sample";
 
-%% PLOT 1
-Plot(1).Layout = [2 2];
+%% PLOT 1 definition 
+
+Plot(1).Layout = [halfrows halfcols];                                     % this is crucial to set the subplot appearance
+
 Plot(1).Title.String = "Example Title";
 
-% Leave an empty string to skip a trace in the legend
-Plot(1).Legend.String = {'Example Legend entry 1', '', 'Example Lengend entry 3'};
+Plot(1).Legend.String = {'Example Legend entry 1', '', 'Example Lengend entry 3'};  % Leave an empty string to skip a trace in the legend
 Plot(1).Legend.Style.Location = 'east';
 Plot(1).Legend.Style.NumColumns = 1;
 
-Plot(1).Vline(1).X = 0.01;
-% Plot(1).Vline(1).Y = [-inf inf];
-Plot(1).Vline(2).X = 0.1;
+Plot(1).Vline(1).X = 0.01;                                  % Example of unlimited vertical line
+Plot(1).Vline(1).Style.Color = "red";
+Plot(1).Vline(2).X = 0.1;                                   % Example of limited vertical line
 Plot(1).Vline(2).Y = [-100 1500];
-
-Plot(1).Vline(1).Style.Color = "red"; 
-Plot(1).Hline(1).X = [0 0.6];
+                       
+Plot(1).Hline(1).X = [0 0.6];                               % Example of limited horizontal line
 Plot(1).Hline(1).Y = 0.5;
 
+% traces array to specify the different traces in the same plot
 
+Plot(1).Trace(1).Y = y1;
+Plot(1).Trace(2).Y = y2;
+Plot(1).Trace(3).Y = y3;
+Plot(1).Trace(1).X = x;
+Plot(1).Trace(2).X = x;
+Plot(1).Trace(3).X = x;
+
+% style options for traces see matlab official documentation for the various options
 
 % Plot(1).Trace(1).Style.Color = Options.Color.Red;
 % Plot(1).Trace(2).Style.Color = Options.Color.Blue;
@@ -58,32 +75,24 @@ Plot(1).Hline(1).Y = 0.5;
 Plot(1).Trace(3).Style.Marker = Options.Marker.Cross;
 Plot(1).Trace(3).Style.MarkerSize = 20;
 
-Plot(1).Trace(1).Y = y1;
-Plot(1).Trace(2).Y = y2;
-Plot(1).Trace(3).Y = y3;
-Plot(1).Trace(1).X = x;
-Plot(1).Trace(2).X = x;
-Plot(1).Trace(3).X = x;
+
+% axis options
 
 Plot(1).YLabel.String= "Sample Ylabel";
 Plot(1).XLabel.String = '';
 % Plot(1).Axis.XTickLabel = '';
 Plot(1).Axis.YLim = [ ];
 Plot(1).Axis.XLim = [ ];
-Plot(1).Axis.XLim = [1 9 ];
+Plot(1).Axis.XLim = [1 9];
 Plot(1).Axis.XScale = 'linear';
 Plot(1).Axis.YScale = 'linear';
-
 Plot(1).Axis.XGrid = 'on';
 Plot(1).Axis.YGrid = 'on';
-
 Plot(1).Axis.XMinorGrid = 'on';
 Plot(1).Axis.YMinorGrid = 'on';
 
 %% PLOT 2
-Plot(2).Rows = 2;
-Plot(2).Columns = 3;
-Plot(2).Layout = [4 2];
+Plot(2).Layout = [fullrows halfcols];
 Plot(2).Title.String = "GRANDE";
 
 Plot(2).Legend.String = {'Example Legend'};
@@ -101,7 +110,7 @@ Plot(2).Trace(3).X = x;
 Plot(2).YLabel.String = "Sample Ylabel";
 Plot(2).XLabel.String = '';
 
-Plot(2).Axis.XLim = [1 9 ];
+Plot(2).Axis.XLim = [1 9];
 
 Plot(2).Zoom(1).X = [2 5];
 Plot(2).Zoom(1).Y = [0.7 0];
@@ -111,14 +120,6 @@ Plot(2).Zoom(1).YPosition = [0.7 -0.5];
 Plot(2).Axis.XGrid = 'on';
 Plot(2).Axis.YGrid = 'on';
 
-% Plot(2).Axis.XMinorGrid = 'on';
-% Plot(2).Axis.YMinorGrid = 'on';
-
-
-
-% Plot(2).Axis.YTick = [ ];
-% Plot(2).Axis.YLim = [ ];
-% Plot(2).Axis.XLim = [ ];
 
 
 % Note(1).Type = 'textbox';
@@ -130,19 +131,16 @@ Plot(2).Axis.YGrid = 'on';
 % Note(1).Style.FitBoxToText  = 'on';
 % Note(1).Style.LineStyle  = 'none';
 % Note(1).Style.Color  = 'Red';
-
-
-% Note(1).Type = 'rectangle';
-
-% Note(1).Text  = 'test array';
+% 
+% 
 % Note(1).Style.LineStyle  = '-.';
 % Note(1).Style.LineWidth  = 5;
 % Note(1).Style.Color  = 'blue';
 % Note(1).Style.FaceColor = 'red';
 % Note(1).Style.FaceAlpha = .2;
 % Note(1).Style.LineStyle = ':';
-
-
+% 
+% 
 % % Note(3).Family = 3;
 % Note(3).Type = 'textarrow';
 % Note(3).XPosition = [0.6 0.3];
@@ -150,30 +148,25 @@ Plot(2).Axis.YGrid = 'on';
 % Note(3).Text  = 'test array arrow';
 % Note(3).Style.LineStyle  = '--';
 % Note(3).Style.Color  = 'red';
-
-
+% 
+% 
 % Plot(2).Note = Note;
 
-% t = (1/16:1/8:1)'*2*pi;
+time = (1/16:1/8:1)'*2*pi;
 
-% Plot(2).Fill(1).X = 0.5*cos(t);
-% Plot(2).Fill(1).Y = 10*sin(t)+10;
-% Plot(2).Fill(1).Style.FaceColor = [0.8 0.7 1];
-% Plot(2).Fill(1).Style.FaceAlpha = .5;
-% Plot(2).Fill(1).Style.LineStyle = ':';
-% 
-% Plot(2).Fill(2).X = 0.5*cos(t);
-% Plot(2).Fill(2).Y = 10*sin(t)+10;
-% Plot(2).Fill(2).Style.FaceColor = [0.8 0.7 1];
+Plot(2).Fill(1).X = 2*cos(time)+3;
+Plot(2).Fill(1).Y = sin(time);
+Plot(2).Fill(1).Style.FaceColor = [0.8 0.7 1];
+Plot(2).Fill(1).Style.FaceAlpha = .5;
+Plot(2).Fill(1).Style.LineStyle = ':';
 
 
 %% PLOT 3
-Plot(3).Rows = 2;
-Plot(3).Columns = 3;
-Plot(3).Layout = [2 2];
+
+Plot(3).Layout = [halfrows halfcols];
 Plot(3).Title.String = "Example Title";
 
-Plot(3).Legend.String = {'Example Legend'};
+Plot(3).Legend.String = {'Example Legend 1st trace'};
 Plot(3).Legend.Style.Location = 'NorthEast';
 Plot(3).Legend.Style.NumColumns = 1;
 
@@ -188,32 +181,19 @@ Plot(3).Area(1).Y = y1;
 Plot(3).Area(1).X = x;
 Plot(3).Area(1).Style.FaceColor = Options.Color.Green;
 
-
-
 Plot(3).YLabel.String = "Sample Ylabel";
 Plot(3).XLabel.String = '';
 
-Plot(3).Axis.XLim = [1 9 ];
+Plot(3).Axis.XLim = [1 9];
 
-% Plot(3).Zoom(1).X = [2 5];
-% Plot(3).Zoom(1).Y = [0.7 0];
-% Plot(3).Zoom(1).XPosition = [6 8];
-% Plot(3).Zoom(1).YPosition = [0.7 -0.5];
 
 Plot(3).Axis.XGrid = 'on';
 Plot(3).Axis.YGrid = 'on';
 
-%% Plot 4 annotation
+%% Plot 4 space reserved for annotation
 Plot(4).Rows = 1;
 Plot(4).Columns = 1;
 Plot(4).Layout = 'south';
-
-% Plot(3).Title.String = "annotation space";
-
-% Plot(3).Legend.String = {'Example Legend'};
-% Plot(3).Legend.Style.Location = 'NorthEast';
-% Plot(3).Legend.Style.NumColumns = 1;
-
 
 Plot(4).Trace(1).Y = y1;
 Plot(4).Trace(1).X = x;
@@ -229,41 +209,19 @@ Plot(4).Axis.Box='off';
 Plot(4).Axis.XColor='none';
 Plot(4).Axis.YColor='none';
 
-
-% Plot(3).YLabel.String = "Sample Ylabel";
-% Plot(3).XLabel.String = '';
-
-% Plot(3).Zoom(1).X = [2 5];
-% Plot(3).Zoom(1).Y = [0.7 0];
-% Plot(3).Zoom(1).XPosition = [6 8];
-% Plot(3).Zoom(1).YPosition = [0.7 -0.5];
-
-Plot(4).Note(1).Type = 'rectangle';
+Plot(4).Note(1).Type = 'textbox';
+Plot(4).Note(1).Text = 'Space reserved for annotations outside the plot area';
 Plot(4).Note(1).XPosition = [2 7];
 Plot(4).Note(1).YPosition = [0.5 0];
-% Plot(3).Note(1).Text  = 'test array';
 Plot(4).Note(1).Style.FitBoxToText  = 'on';
-% Plot(3).Note(1).Style.LineStyle  = 'none';
 Plot(4).Note(1).Style.Color  = 'Red';
 
-
-% Plot(3) = Plot(1);
-
-% Plot = Plot(1:3);
 
 %% PLOT
 fig1 = figure('Name', ImageTitle, 'WindowState', 'maximized'); 
 drawnow;
-layout = [0 0];
 
-for i=1:length(Plot)
-    if isnumeric(Plot(i).Layout)
-layout = layout + Plot(i).Layout;
-    end
-end 
-
-
-t = tiledlayout(4, 4,'Padding','tight','TileSpacing','tight'); %tight
+t = tiledlayout(nrows, ncols,'Padding','tight','TileSpacing','tight'); 
 t.Units = Options.Units;
 t.OuterPosition = [0 0 Options.Width Options.Height];
 
@@ -271,17 +229,6 @@ for i=1:length(Plot)
   axes(i) = nexttile(t, Plot(i).Layout);
   PLECSplot(Plot(i), Options)
 end
-
-
-%   nexttile(t, [3,1]);
-%   PLECSplot(Plot(1), Options)
-%   nexttile(t, [3,1]);
-% PLECSplot(Plot(2), Options)
-%   nexttile(t, [1,1]);
-% PLECSplot(Plot(3), Options)
-
-% linkaxes(axes, 'x') % Options: x, y, xy, off
-
 
 %% SAVE
 plot2file(fig1, Options, ImageTitle);
